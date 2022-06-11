abef495ed16e5f8f1ebcc16a2eb4a57c35ae9c52
queirozime <carloshenricco@gmail.com>
2022-06-11 20:16:04 -0300

    fix missing char in script

--- test_laplace_jacobi_4.py
+++ test_laplace_jacobi_4.py
@@ -44,7 +44,7 @@ def loop_time_step(u):
 #----------------------
 # Function: loop_solver
 #----------------------
-@dectimer.bench_time(3, sys.argv[1])
+@dectimer.bench_time(3, sys.argv[1], environment)
 def loop_solver(n):
     """
         Find the desired numerical solution using loops
